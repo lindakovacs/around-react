8b0901bf41182434fc35bc8abbbc6b2b98ce0473
Linda Kovacs <lindaprovenza@yahoo.com>
2020-08-30 10:38:01 -0400

    Updates

---
+++ precache-manifest.a9d6b482430a24244b49804011d18f68.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890b0764848b5e060cf345e540b14621",
+    "url": "/around_react/index.html"
+  },
+  {
+    "revision": "b00b6846e5c506c0e57b",
+    "url": "/around_react/static/css/main.1f28f9c5.chunk.css"
+  },
+  {
+    "revision": "452e579f5df8fdfc3251",
+    "url": "/around_react/static/js/2.6e4d6e10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/around_react/static/js/2.6e4d6e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00b6846e5c506c0e57b",
+    "url": "/around_react/static/js/main.bbdf026c.chunk.js"
+  },
+  {
+    "revision": "0acf1a6bdfaed93ffaee",
+    "url": "/around_react/static/js/runtime-main.b07083a0.js"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/around_react/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/around_react/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "38bc51bccb7dd07b2752c11888cbdc89",
+    "url": "/around_react/static/media/Inter-ExtraBold.38bc51bc.woff"
+  },
+  {
+    "revision": "92d16aee8fb5f5c5cfd660b2d07e1148",
+    "url": "/around_react/static/media/Inter-ExtraBold.92d16aee.woff2"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/around_react/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/around_react/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/around_react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/around_react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/around_react/static/media/add.b2fce4ac.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/around_react/static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/around_react/static/media/edit-avatar.d476a8b8.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/around_react/static/media/edit.6bfdb354.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/around_react/static/media/heart-black.5bbb05f7.svg"
+  },
+  {
+    "revision": "30f5099ae60f46326fd48cc86c724b5f",
+    "url": "/around_react/static/media/heart.30f5099a.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/around_react/static/media/logo.8f7611ae.svg"
+  },
+  {
+    "revision": "f202ad3053ae4410e6d246994df87581",
+    "url": "/around_react/static/media/trash-can.f202ad30.svg"
+  }
+]);
